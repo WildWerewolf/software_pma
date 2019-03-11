027e435232cc44e4ade1f9121c0fc599e118ca81
Rubens Vieira <rubensvin@hotmail.com>
2019-03-10 23:08:19 -0300

    atualizado o arquivo listagem_clientes com listagemAlfabetica

--- listagem_clientes.php
+++ listagem_clientes.php
@@ -19,10 +19,63 @@
     <div class="titulo-secao">
         Listagem de Clientes
     </div>
+
+    <div class="alfabeto">
+
+        <?php
+            
+            /**
+             * O código abaixo cria um alfabeto e torna as letras em links que indicam a inicial dos clientes 
+             * que o usuário deseja listar.
+             *  e essa é uam diferença nova só pra testar o commit
+             * Rubens
+             * 
+             */
+            $letra = 'a';
+            $cont = 0;
+            while($cont < 26){
+
+                if(isset($_GET['letra'])){
+                    $letraClicada = strtolower($_GET['letra']);
+                    /**
+                     * a condição acima verifica se já há uma letra selecionada
+                     */
+                    if($letra == $letraClicada){
+                        /**
+                         * se a ser listada já estiver selecionada, ela entra na classe css "letra_clicada"
+                         * essa classe aumenta o tamanho dessa única letra em 5px;
+                         * 
+                         * Rubens
+                         */
+                        echo '<a class="letra_clicada" href="listagem_clientes.php?letra='.$letra.'">'.$letra.'</a>     ';
+                    }else{
+                        /**
+                         * se a letra não foi selecionada, ela é listada com tamanho normal
+                         */
+                        echo '<a href="listagem_clientes.php?letra='.$letra.'">'.$letra.'</a>     ';
+                    }                    
+
+                }else{
+                    /**
+                     * se não houver letra selecionada ainda, todas elas ficam com o tamanho normal
+                     */
+                    echo '<a href="listagem_clientes.php?letra='.$letra.'">'.$letra.'</a>     ';
+                }
+
+                $letra++;
+                $cont++;
+            }
+        
+        ?>
+ 
+    </div>
+
     <?php 
+    if (isset($_GET['letra'])) {
         require_once 'classes/Cliente.php';
         $cliente = new Cliente();
-        $cliente->listarClientes(0);
+        $cliente->listagemAlfabetica($_GET['letra']);
+    }
     ?>
 
 </body>
